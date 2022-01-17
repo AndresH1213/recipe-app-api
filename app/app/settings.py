98a179b2ec0c738053d66548db6b71fe5841b098
Andres Hernandez <edanhebla1213@hotmail.com>
2022-01-17 15:33:48 -0500

    Added working list tags endpoint

--- app/app/settings.py
+++ app/app/settings.py
@@ -41,7 +41,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     
     'core.apps.CoreConfig',
-    'user.apps.UserConfig'
+    'user.apps.UserConfig',
+    'recipe.apps.RecipeConfig',
 ]
 
 MIDDLEWARE = [
